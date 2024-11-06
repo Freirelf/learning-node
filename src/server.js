f06ebfacca34687dcf13fb933848e3eecb201481
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-11-06 16:18:20 -0300

    application structure and stream read, write and HTTP

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import http from 'node:http';
+
+const users = [];
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if(method === 'GET' && url === '/users') {
+
+    return res.setHeader('Content-Type', 'application/json').end(JSON.stringify(users));
+  }
+
+  if(method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'João',
+      email: 'zjz0v@example.com'
+    })
+
+    return res.writeHead(201).end();
+  }
+
+  return res.writeHead(404).end('Not Found');
+});
+
+server.listen(3333);
