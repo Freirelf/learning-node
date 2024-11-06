36399a605567604e07948a2f4bf6bd2599e7347d
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-11-06 18:11:42 -0300

    Buffers in node

---
+++ streams/buffer.js
@@ -0,0 +1,9 @@
+const buf = Buffer.from('hello');
+
+console.log(buf.toJSON());
+
+// **é um objeto que armazena dados binários em memória temporária**
+
+// . É uma estrutura de dados que permite a manipulação direta da memória bruta, o que o torna eficiente em termos de desempenho.
+
+// . O buffer é uma estrutura de dados que armazena dados em um array de bytes.
